ab296ddd3cf027d96b3986c9a0d6ccf0c7ea8c0a
Alain FERRARO <afer92@hotmail.com>
2017-02-25 19:03:51 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name = 'ofxutils',
+	version = '1.0',
+	author = 'afer92',
+	author_email = 'ferraro.alain@free.fr',
+	
+	url = "http://github.com/afer92/ofxutils",
+	keywords = 'ofx ofc qif utilities',
+	description = 'Provide tools for financial file parsing and conversion.',
+	license = 'GNU General Public License (GPL)',
+	packages = ['ofxtools'],
+	install_requires = ['BeautifulSoup'],
+	
+	classifiers = [
+		'Development Status :: 4 - Beta',
+		'License :: OSI Approved :: GNU General Public License (GPL)',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.4',
+	],
+)
